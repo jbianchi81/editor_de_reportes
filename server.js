88f4dac183d6b6627ef4603655aca8f869e64575
jbianchi81 <jbianchi@ina.gob.ar>
2025-04-23 18:11:01 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.json({ limit: '5mb' }));
+
+// Serve the saved HTML content
+app.get('/content', (req, res) => {
+  fs.readFile('public/saved.html', 'utf8', (err, data) => {
+    if (err) return res.send('');
+    res.send(data);
+  });
+});
+
+app.get('/template', (req, res) => {
+  fs.readFile('public/template.html', 'utf8', (err, data) => {
+    if (err) return res.send('');
+    res.send(data);
+  });
+});
+
+// Save new HTML content
+app.post('/save', (req, res) => {
+  const html = req.body.html;
+  fs.writeFile('public/saved.html', html, err => {
+    if (err) return res.status(500).send('Failed to save');
+    res.send('Saved successfully!');
+  });
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
